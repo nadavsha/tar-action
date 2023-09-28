0c0e40a38c071dd7d328091143aa4e9daacba158
Shabat01 <50592090+nadavsha@users.noreply.github.com>
2023-09-28 20:59:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-const core = require("@actions/core");
-const tar = require("tar");
-const fs = require("fs");
+const core = require('@actions/core');
+const tar = require('tar');
+const fs = require('fs');
 
-( async () => {
-  try {
-    const cwd = core.getInput("cwd");
-    const command = core.getInput("command", { required: true });
-    const files = await fs.readdirSync('.');
-    const outPath = core.getInput("outPath");
-  
+try {
+    const cwd = core.getInput('cwd');
+    const command = core.getInput('command', { required: true });
+    const files = fs.readdirSync('.');
+    const outPath = core.getInput('outPath');
+
     const listOfFiles = files;
-  
+
     switch (command) {
-      case "c": {
-        if(!outPath)
-          throw new Error("Please specify an out path");
-        tar
-          .c({ cwd, gzip: true, sync: true }, listOfFiles)
-          .pipe(fs.createWriteStream(outPath));
-        break;
-      }
-      case "x": {
-        if(files.length !== 1)
-          throw new Error("Only one file can be extracted");
-        tar
-          .x({ C: cwd, sync: true, file: files[0] });
-        break;
-      }
-      default:
-        throw new Error(`Unsupported command: ${command}`);
+        case 'c': {
+            if (!outPath) throw new Error('Please specify an out path');
+            tar.c({ cwd, gzip: true, sync: true }, listOfFiles).pipe(
+                fs.createWriteStream(outPath)
+            );
+            break;
+        }
+        case 'x': {
+            if (files.length !== 1)
+                throw new Error('Only one file can be extracted');
+            tar.x({ C: cwd, sync: true, file: files[0] });
+            break;
+        }
+        default:
+            throw new Error(`Unsupported command: ${command}`);
     }
-  
-    core.setOutput("done", true);
-  } catch (error) {
+
+    core.setOutput('done', true);
+} catch (error) {
     core.setFailed(error.message);
-})()
+}
